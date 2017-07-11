d6db7782f22a7336dc986f9fd674558483ac635e
mulatya <emmanuelmulatya8@gmail.com>
2017-07-11 09:36:28 +0300

    changed the readme

--- app/meal-list.component.js
+++ app/meal-list.component.js
@@ -22,18 +22,18 @@ var mealListComponent = (function () {
         this.clickSender.emit(mealToEdit);
     };
     __decorate([
-        core_1.Input(), 
+        core_1.Input(),
         __metadata('design:type', Array)
     ], mealListComponent.prototype, "childmealList", void 0);
     __decorate([
-        core_1.Output(), 
+        core_1.Output(),
         __metadata('design:type', Object)
     ], mealListComponent.prototype, "clickSender", void 0);
     mealListComponent = __decorate([
         core_1.Component({
             selector: 'meal-list',
             template: "\n    <select (change)=\"onChange($event.target.value)\">\n      <option value=\"all\" selected=\"selected\">Show All</option>\n      <option value=\"highCalories\">Show high Calories</option>\n      <option value=\"lowCalories\" >Show low calories</option>\n    </select>\n    <div class=\"meal-list \" *ngFor=\"let currentmeal of childmealList | completeness:selectedCompleteness\">\n <div >\n      <h3><strong>Food :{{ currentmeal.meal }}</strong></h3>\n      </div>\n      <div >\n      <h5><strong>Details :</strong> {{ currentmeal.description }}</h5>\n </div>\n <div >\n      <p><strong>Calories :</strong> {{ currentmeal.calories }}</p>\n      <button (click)=\"editButtonHasBeenClicked(currentmeal)\">Edit</button>\n      </div>\n\n    </div>\n\n  "
-        }), 
+        }),
         __metadata('design:paramtypes', [])
     ], mealListComponent);
     return mealListComponent;
